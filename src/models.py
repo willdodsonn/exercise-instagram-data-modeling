66c671720cc19bd6e9a101363a76c6222e579680
willdodsonn <sodlax38@gmail.com>
2022-03-13 16:13:15 +0000

    sorry maikel, this is copy paste and not mine. i'm creating my own I just wanted to see it work XD

--- src/models.py
+++ src/models.py
@@ -10,39 +10,31 @@ Base = declarative_base()
 
 
 
-class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
-    firstname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-    comment = relationship(Comment)
-
-class Post(Base):
-    __tablename__ = 'post'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey ('user_id'))
-    author = relationship(User)
-    # def to_dict(self):
-    #     return {}
-
 class Comment(Base):
-    __tablename__ = 'user'
+    __tablename__ = "comment"
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
+    # author = relationship(User)
 
-class Follower(Base):
-    __tablename__ = 'follower'
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    followed = Column(String(250),ForeignKey('user.id'))
-    follower = Column(String(250),ForeignKey('user.id'))
+    username = Column(String(250), unique=True, nullable=False)
+    firstname = Column(String(250), unique=False, nullable=False)
+    lastname = Column(String(250), unique=False, nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    comment = relationship(Comment)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    author = relationship(User)
+
 
 
 class Media(Base):
@@ -51,6 +43,15 @@ class Media(Base):
     media_type = Column(String(250), nullable =False)
     url = Column(String(250), unique = True, nullable =False)
     post_id = Column(Integer, ForeignKey('post.id'))
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    followed = Column(String(250),ForeignKey('user.id'))
+    follower = Column(String(250),ForeignKey('user.id'))
+
+
 
 ## Draw from SQLAlchemy base
 try:
